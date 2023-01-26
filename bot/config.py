ecd56312e0c8531ce022b3a8653707fc52742b9e
121189493+Nubuki-all@users.noreply.github.com <121189493+Nubuki-all@users.noreply.github.com>
2023-01-26 14:27:05 +0100

    fix

--- bot/config.py
+++ bot/config.py
@@ -30,7 +30,7 @@ try:
     )
     ICON = config("ICON", default="https://te.legra.ph/file/462b5a002f80bdf8a1ec1.png")
     LOG_CHANNEL = config("LOG_CHANNEL", default="")
-    DBNAME = config("DBNAME", default=int(BOT_TOKEN.split(":", 1)[0]))
+    DBNAME = config("DBNAME", default=str(BOT_TOKEN.split(":", 1)[0]))
     DATABASE_URL = config("DATABASE_URL", default="")
     FCHANNEL = config("FCHANNEL", default="")
     FCHANNEL_STAT = config("FCHANNEL_STAT", default="")
