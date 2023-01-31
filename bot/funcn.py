53eaa0725a1a20002448437a0f70a9e9113737f5
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2023-01-31 23:00:05 +0000

    pyLint: auto-fixes

--- bot/funcn.py
+++ bot/funcn.py
@@ -122,14 +122,16 @@ def stdr(seconds: int) -> str:
 
 
 def time_formatter(seconds: float) -> str:
-    """ humanize time """
+    """humanize time"""
     minutes, seconds = divmod(int(seconds), 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "")
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+    )
     return tmp[:-2]
 
 
