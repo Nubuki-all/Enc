ab91172815cb883e0e87852ef59dc31b971d3cdb
121189493+Nubuki-all@users.noreply.github.com <121189493+Nubuki-all@users.noreply.github.com>
2023-09-06 06:54:25 +0100

    Update local deploy script

--- local_deploy.sh
+++ local_deploy.sh
@@ -4,6 +4,12 @@ RED='\033[0;31m\e[1m'
 NC='\033[0m'
 GREEN='\e[32m\e[1m'
 
+# Check if python3 is installed.
+if which python3 >/dev/null; then
+    echo -e "${GREEN}python3 is installed…${NC}"
+else
+    echo -e "${RED}python3 not is installed.\nAborting…${NC}"
+    exit 1
 
 # Check the Linux distribution
 if grep -qi "fedora" /etc/os-release; then
@@ -22,7 +28,7 @@ if grep -qi "fedora" /etc/os-release; then
 
 elif grep -qi "ubuntu\|debian" /etc/os-release; then
     # Install the package on Ubuntu or Debian using apt
-    #remove the '#' from line 4 if you want handbrake-cli support
+    #remove the '#' from line 37 if you want handbrake-cli support
     echo -e "${GREEN}Apt package manger detected!${NC}"
     echo -e "${GREEN}Downloading and installing system dependencies required to run bot…${NC}"
     sleep 2
@@ -30,10 +36,10 @@ elif grep -qi "ubuntu\|debian" /etc/os-release; then
         echo -e "${GREEN}sudo is installed.\nInstalling other requirements…${NC}"
     else
         echo -e "${GREEN}sudo is not installed\nInstalling alongside other requirements…${NC}"
-        apt -y -qq update && apt install sudo
+        apt -y -qq update && apt install sudo -y
     fi
     sleep 2
-    sudo apt -y -qq update && sudo apt -y -qq install -y git wget curl aria2 pv jq python3-dev python3-pip mediainfo
+    sudo apt -y -qq update && sudo apt -y -qq install -y git wget curl aria2 pv jq python3-pip mediainfo
     #sudo apt -qq install handbrake-cli
 
 
@@ -52,9 +58,9 @@ if which ffmpeg >/dev/null; then
 else
     echo -e "${GREEN}ffmpeg is not installed.${NC}"
     echo -e "${GREEN}Downloading and installing latest ffmpeg…${NC}"
-    wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz && tar -xvf *xz && cp *5.1/bin/* /usr/bin
+    wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-6.0.tar.xz && tar -xvf *xz && cp *6.0/bin/* /usr/bin
     echo -e "${GREEN}Cleaning up…${NC}"
-    rm -rf *xz && rm -rf *5.1
+    rm -rf *xz && rm -rf *6.0
 fi
 
 sleep 2
