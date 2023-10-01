8b49996c8e6fd4b5a05e2c8b177a72c9ba7e3a70
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2023-10-01 13:37:24 +0000

    pyLint: auto-fixes

--- bot/workers/encoders/encode.py
+++ bot/workers/encoders/encode.py
@@ -5,9 +5,7 @@ from bot.fun.emojis import enmoji
 from bot.utils.bot_utils import code, decode
 from bot.utils.log_utils import logger
 
-def_enc_msg = (
-    "**Currently Encoding {}:**\n└`{}`\n\n**⏳This Might Take A While⏳**"
-)
+def_enc_msg = "**Currently Encoding {}:**\n└`{}`\n\n**⏳This Might Take A While⏳**"
 
 
 class Encoder:
