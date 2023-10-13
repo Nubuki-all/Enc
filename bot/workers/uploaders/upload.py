028fd306a9b9ac7d0a6e361dcc31f37b49e19fd3
121189493+Nubuki-all@users.noreply.github.com <121189493+Nubuki-all@users.noreply.github.com>
2023-10-14 00:33:56 +0100

    final ui update.

--- bot/workers/uploaders/upload.py
+++ bot/workers/uploaders/upload.py
@@ -87,17 +87,16 @@ class Uploader:
             speed = current / diff
             time_to_completion = time_formatter(int((total - current) / speed))
 
-            progress = f"{bar}\n" "{0}{1}\n{2}\n<b>Progress:</b> `{3}%`\n".format(
+            progress = f"{bar}\n" "{0}{1}\n{2}\n{3}\n<b>Progress:</b> `{4}%`\n".format(
                 "".join([fin_str for i in range(math.floor(percentage / 10))]),
                 "".join([unfin_str for i in range(10 - math.floor(percentage / 10))]),
                 bar,
+                file_info,
                 round(percentage, 2),
             )
 
             tmp = (
                 progress
-                + file_info
-                + "\n"
                 + "`{0} of {1}`\n**Speed:** `{2}/s`\n**ETA:** `{3}`\n**Elapsed:** `{4}`\n".format(
                     hbs(current),
                     hbs(total),
