395641f5cb9ce1564bbdcda87af05fe4c09da1eb
121189493+Nubuki-all@users.noreply.github.com <121189493+Nubuki-all@users.noreply.github.com>
2023-11-06 05:59:58 +0100

    .

--- bot/workers/handlers/queue.py
+++ bot/workers/handlers/queue.py
@@ -894,7 +894,7 @@ async def edit_queue(event, args, client):
     try:
         cust_fil = cust_v = str()
         flag, args = get_args(
-            "-e",
+           ["-e", "store_true"], 
             "-f",
             "-n",
             "-q",
