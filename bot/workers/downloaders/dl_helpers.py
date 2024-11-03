5a4215d06cb3ed52bae268c0fa48a1502c97ef58
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-11-03 18:35:34 +0100

    update dl_helpers.py

--- bot/workers/downloaders/dl_helpers.py
+++ bot/workers/downloaders/dl_helpers.py
@@ -201,7 +201,7 @@ async def cache_dl(check=False, cached=False):
     if check:
         return _bot.cached
     if cached:
-        _bot.cached = True
+        return _bot.cached = True
     try:
         queue = get_queue()
         chat_id, msg_id = list(queue.keys())[1]
